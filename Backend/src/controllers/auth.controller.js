882a8ad41c824c051cf194ad7a6b06a71c52f549
prakashchand07 <prakash0719pc@gmail.com>
2026-03-18 22:48:58 +0530

    minor

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -10,7 +10,7 @@ const login = async (req, res, next) => {
 
         const user = await User.findOne({ email: email.toLowerCase() })
             .select('+password')
-            .populate('company', 'name email logo currency invoicePrefix isActive plan');
+            .populate('company', 'name email logo currency invoicePrefix isActive plan industryType');
 
         if (!user) {
             return res.status(401).json({ success: false, message: 'Invalid email or password.' });
@@ -55,7 +55,7 @@ const getMe = async (req, res, next) => {
     try {
         const user = await User.findById(req.user._id)
             .select('-password')
-            .populate('company', 'name email logo currency invoicePrefix plan planExpiry isActive');
+            .populate('company', 'name email logo currency invoicePrefix plan planExpiry isActive industryType');
         res.status(200).json({ success: true, data: user });
     } catch (error) {
         next(error);
